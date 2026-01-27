5c5a6100921f99480cc90ec9d7c0f11e7fe13b0b
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2026-01-27 09:12:22 +0100

    edited code base

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,21 +1,21 @@
-const io = require("socket.io")(8000, {
-  cors: {
-    origin: "*",
-  },
-});
+const io = require("socket.io")(8000, { cors: { origin: "*" } });
 
 let totalVotes = 0;
 let votingPolls = {
   html: 0,
   css: 0,
-  js: 0,
+  javascript: 0,
+  react: 0,
   python: 0,
 };
 
-//  Send Current Polls Data to New User
 io.on("connection", (socket) => {
+  //Send Current Data of Votes to user when visited the site
+  socket.emit("update", { votingPolls, totalVotes });
+
   socket.on("send-vote", (voteTo) => {
     totalVotes += 1;
+    console.log(voteTo);
     votingPolls[voteTo] += 1;
     socket.broadcast.emit("receive-vote", { votingPolls, totalVotes });
     socket.emit("update", { votingPolls, totalVotes });
